5ead4de58c019ce3a66084e9dec1fa3c47014352
Jay Hayes <ur@iamvery.com>
2016-03-19 13:46:05 -0500

    Start cowboy in the application callback
    Again, there is a lot of code needed here to essentially say "start
    cowboy using these routes".

--- lib/web_server.ex
+++ lib/web_server.ex
@@ -1,4 +1,22 @@
 defmodule WebServer do
+  def start(_type, _args) do
+    start_cowboy
+  end
+
+  defp start_cowboy do
+    scheme = :http
+    number_of_processes_in_pool = 100
+    protocol_options = [port: 8080]
+    cowboy_options = [env: [dispatch: dispatch]]
+
+    {:ok, _pid} = :cowboy.start_http(
+      scheme,
+      number_of_processes_in_pool,
+      protocol_options,
+      cowboy_options
+    )
+  end
+
   defp dispatch do
     all_hosts = :_
     root_route = {"/", RequestHandler, []}
