da341a843ed2e86412041be266acb5fa6a688294
Jay Hayes <ur@iamvery.com>
2016-03-19 13:44:58 -0500

    Define our cowboy dispatch in a private method
    This is a lot to grok. Basically this is setting up routing for cowboy.
    Our server will route two requests, "/" and "/foo", both to the
    RequestHandler.

--- lib/web_server.ex
+++ lib/web_server.ex
@@ -1,2 +1,11 @@
 defmodule WebServer do
+  defp dispatch do
+    all_hosts = :_
+    root_route = {"/", RequestHandler, []}
+    foo_route = {"/foo", RequestHandler, []}
+
+    :cowboy_router.compile([
+      {all_hosts, [root_route, foo_route]}
+    ])
+  end
 end
